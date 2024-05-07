6fcb0ecbbfb29aa2e88d594026f8c72e53c831f5
Rajan Kumar Singh <159889339+RajanSingh08@users.noreply.github.com>
2024-05-07 12:43:11 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,10 @@ console.log(process.env.FRONTEND_URL)
   };
 
   
-  app.use(cors(corsOptions));
+  // app.use(cors(corsOptions));
+app.use(cors({
+  origin: 'https://6639d34d692e08ad5bf6845f--gregarious-muffin-342ff7.netlify.app'
+}));
   
   
 
